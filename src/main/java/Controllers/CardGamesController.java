6c90ba9b3e56e9f490add3dc0554fec1ed946bdd
Anna Arnold <anna.arnold.96@gmail.com>
2024-10-11 15:48:21 +0100

    game accessed through a cli

---
+++ src/main/java/Controllers/CardGamesController.java
@@ -0,0 +1,16 @@
+package Controllers;
+
+import higherOrLower.HigherOrLower;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "cardGames", subcommands = {HigherOrLower.class})
+
+public class CardGamesController implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Card games to play");
+            System.out.println("higherOrLower: Higher or Lower game");
+        }
+    }
+
--- src/main/java/higherOrLower/HigherOrLower.java
+++ src/main/java/higherOrLower/HigherOrLower.java
@@ -5,53 +5,59 @@ package higherOrLower;
 
 // import com.fasterxml.jackson.databind.ObjectMapper;
 
+import kotlin.jvm.Throws;
+import picocli.CommandLine;
 
-public class HigherOrLower {
+@CommandLine.Command(name="higherOrLower")
+public class HigherOrLower implements Runnable {
 
-    public static void main(String[] args) throws Exception {
+    public void run() {
 
         boolean winning = true;
         HLMethods methods = new HLMethods();
+        try {
+            // Create the deck
+            Deck deck = CardDeckAPIs.getADeck(1);
+            String deckId = deck.getDeckId();
 
-        // Create the deck
-        Deck deck = CardDeckAPIs.getADeck(1);
-        String deckId = deck.getDeckId();
+            // Draw the card
+            Card cardDrawn = CardDeckAPIs.drawCards(1, deckId);
 
-        // Draw the card
-        Card cardDrawn = CardDeckAPIs.drawCards(1, deckId);
-
-        // ------------ DEBUGGING -------------
-        // System.out.println(cardDrawn);
-        // System.out.println(deck);
-
-        // Get card value & show the card
-        String cardValue = cardDrawn.getValue();
-        methods.printCardDrawn(cardValue,cardDrawn.getSuit());
-
-        int intCardValue = cardDrawn.setFaceCards(cardValue);
-
-        // Repeat until lost
-        while (winning == true) {
-
-            // User input
-            String userInput = methods.userInput();
-            methods.userChoice(userInput);
-
-            int lastCardValue = intCardValue;
-
-            // Draw next card
-            cardDrawn = CardDeckAPIs.drawCards(1, deckId);
+            // ------------ DEBUGGING -------------
+            // System.out.println(cardDrawn);
+            // System.out.println(deck);
 
             // Get card value & show the card
-            cardValue = cardDrawn.getValue();
-            methods.printCardDrawn(cardValue,cardDrawn.getSuit());
+            String cardValue = cardDrawn.getValue();
+            methods.printCardDrawn(cardValue, cardDrawn.getSuit());
 
-           // Set int for face cards
-           intCardValue = cardDrawn.setFaceCards(cardValue);
+            int intCardValue = cardDrawn.setFaceCards(cardValue);
 
-           // Check end game criteria
-           winning = methods.endGameCheck(intCardValue,lastCardValue,userInput);
+            // Repeat until lost
+            while (winning == true) {
 
+                // User input
+                String userInput = methods.userInput();
+                methods.userChoice(userInput);
+
+                int lastCardValue = intCardValue;
+
+                // Draw next card
+                cardDrawn = CardDeckAPIs.drawCards(1, deckId);
+
+                // Get card value & show the card
+                cardValue = cardDrawn.getValue();
+                methods.printCardDrawn(cardValue, cardDrawn.getSuit());
+
+                // Set int for face cards
+                intCardValue = cardDrawn.setFaceCards(cardValue);
+
+                // Check end game criteria
+                winning = methods.endGameCheck(intCardValue, lastCardValue, userInput);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
         }
     }
 }
